8f9a739364955f88498fa679e611e1266ce14184
srivastavprashant <prashant.a.srivastav007@gmail.com>
2022-05-08 18:28:12 +0530

    Initial commit

---
+++ TalkSane/src/main/java/com/talksane/chat/TalkSane/TalkSaneApplication.java
@@ -0,0 +1,13 @@
+package com.talksane.chat.TalkSane;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TalkSaneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TalkSaneApplication.class, args);
+	}
+
+}
---
+++ TalkSane/src/test/java/com/talksane/chat/TalkSane/TalkSaneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talksane.chat.TalkSane;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TalkSaneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
